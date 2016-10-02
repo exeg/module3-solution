195ddfe2d0db268e1e32dd9ad6e1699c7841e488
Exeg <exexeg@gmail.com>
2016-10-02 05:24:14 +0300

    'Final'

---
+++ app.js
@@ -0,0 +1,94 @@
+(function () {
+'use strict';
+
+angular.module('NarrowItDownApp', [])
+
+
+.controller('NarrowItDownController', NarrowItDownController)
+.service('MenuSearchService', MenuSearchService)
+.constant('ApiBasePath', " https://davids-restaurant.herokuapp.com/menu_items.json")
+.directive('foundItems', FoundItemsDirective);
+    
+function FoundItemsDirective() {
+  var ddo = {
+   templateUrl: 'menuList.html',
+    scope: {
+      items: '<',
+      onRemove: '&'
+    },
+    controller: FoundItemsDirectiveController,
+    controllerAs: 'menu',
+    bindToController: true
+  };
+
+  return ddo;
+}
+    
+
+
+function FoundItemsDirectiveController() {
+  var menu = this;
+
+}
+
+NarrowItDownController.$inject = ['MenuSearchService','$scope'];
+function NarrowItDownController(MenuSearchService, $scope) {
+    var menu = this;
+    menu.found = [];
+    menu.getMenuItems = function (searchTerm) {
+        
+    var promise = MenuSearchService.getMatchedMenuItems(searchTerm);
+    promise.then(function (response) {
+        if (response.length !== 0) {
+            menu.found = response;
+        }
+        else {
+            $scope.errorMessage = "Nothing found";
+            
+        }
+        })
+    
+  };
+    
+
+    menu.removeItem = function (itemIndex) {
+            menu.found.splice(itemIndex, 1);
+        };
+
+
+}
+
+
+MenuSearchService.$inject = ['$http', 'ApiBasePath']
+function MenuSearchService($http, ApiBasePath) {
+  var service = this;
+  var items = [];
+
+
+service.getMatchedMenuItems = function (searchTerm) {
+    return $http({
+        method: "GET",
+        url: (ApiBasePath)
+        }).then(function (result) {
+        // process result and only keep items that match
+        var fs = [];
+        console.log(result.data);
+        for (var i = 0; i < result.data.menu_items.length; i++) {
+          var descr = result.data.menu_items[i].description;
+          if ((descr.toLowerCase()).indexOf(searchTerm) !== -1) {
+            fs.push(result.data.menu_items[i]);
+          }
+        }
+
+    // return processed items
+    console.log(fs);
+    return fs;
+});
+    
+
+    };
+    
+
+    
+
+})();
